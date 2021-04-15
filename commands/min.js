f4f03a03ae60dfab6211b6f1317e91356a5c0154
doinkythederp <mcclelll@icloud.com>
2021-04-14 20:05:49 -0700

    Finish min.js
    Finished adding unicode codes to min.js

--- commands/min.js
+++ commands/min.js
@@ -1,9 +1,9 @@
 // Custom command for converting your JS to be put in a CC
 // Not ready - needs unicode codes inserted
 
-let startBracket = "\\uSB";
-let endBracket = "\\uEB";
-let quote = "\\uQUOT";
+let startBracket = "\\u007B";
+let endBracket = "\\u007D";
+let quote = "\\u0022";
 let converted = args.str.replace(new RegExp('\\' + eval(startBracket), 'g'), startBracket).replace(new RegExp('\\' + eval(startBracket), 'g'), endBracket).replace(/\n/g, "\\n").replace(/"/g, quote);
 eval(startBracket) + 'eval("' + converted + '")' + eval(endBracket);
 
@@ -11,5 +11,5 @@ eval(startBracket) + 'eval("' + converted + '")' + eval(endBracket);
 // Minified
 
 // V Copy/Paste Me! V
-// (prefix)cc create minify {let startBracket="\\uSB";let endBracket="\\uEB";let quote="\\uQUOT";let converted=args.str.replace(new RegExp('\\'+eval(startBracket),'g'),startBracket).replace(new RegExp('\\'+eval(startBracket),'g'),endBracket).replace(/\n/g,"\\n").replace(/"/g,quote);eval(startBracket)+'eval("'+converted+'")'+eval(endBracket);}
+// (prefix)cc create minify {let startBracket="\\u007B";let endBracket="\\u007D";let quote="\\u0022";let converted=args.str.replace(new RegExp('\\'+eval(startBracket),'g'),startBracket).replace(new RegExp('\\'+eval(startBracket),'g'),endBracket).replace(/\n/g,"\\n").replace(/"/g,quote);eval(startBracket)+'eval("'+converted+'")'+eval(endBracket);}
 // ^       ^        ^
